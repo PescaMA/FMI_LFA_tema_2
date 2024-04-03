223523852fdc3baed7d005f55269f020ef093402
PescaMA <pescariu@real.cpp>
2024-04-03 10:14:20 +0300

    refactor into multiple smaller functions. Fixed some bugs

--- main.cpp
+++ main.cpp
@@ -2,9 +2,8 @@
 #include <fstream>
 #include <unordered_map>
 #include <unordered_set>
-#include <stack>
-#include <deque>
 #include <vector>
+#include <algorithm>
 
 class FA{ /// abstract implementation of a finite automata
 protected:
@@ -16,9 +15,14 @@ protected:
 
 public:
     FA(){}
+    bool inline acceptedLetter(char ch){
+        return (ch >= 'A' && ch <='Z') ||
+               (ch >= 'a' && ch <='z') ||
+               (ch >= '0' && ch <='9');
+    }
     void read(std::istream& in){/// input data format is present in requirements.txt
         int n,m;
-        std::deque<int> states;
+        std::vector<int> states;
         std::string listFinals;
         int nrFinalStates;
         /// Didn't use the above variables so I didn't put them as members.
@@ -48,80 +52,142 @@ public:
         return in; /// used read so I could write that inside the class.
    }
    friend std::ostream& operator<<(std::ostream& out, FA& myFA){
-        for(auto el:myFA.edges){
-            out << el.first << ": ";
-            for(auto edge:el.second){
-                for(auto next:edge.second)
-                    out << " -" << edge.first << "-> " << next;
-            }
+       std::vector<int> nodes;
+        for(auto el:myFA.edges)
+            nodes.push_back(el.first);
+
+        std::sort(nodes.begin(),nodes.end());
+
+
+        for(auto node:nodes){
+            std::vector<std::pair<int,char> > sortedEdges;
+            for(auto edge:myFA.edges[node])
+                for(auto edgeNode:edge.second)
+                    sortedEdges.emplace_back(edgeNode,edge.first);
+
+            std::sort(sortedEdges.begin(),sortedEdges.end());
+
+            out << node << ":";
+            for(auto edge:sortedEdges)
+                out << " -" << edge.second << "-> " << edge.first;
             out << '\n';
         }
+            /*
+            for(auto edge:el.second){
+                for(auto next:edge.second)
+                    out << " -" << edge.first << "-> " << next;*/
+
         return out; /// used read so I could write that inside the class.
    }
 };
 
 class l_NFA : public FA{
 
-    /// returns start and finish state
-    std::pair<int,int> readRegex(const std::string& regExp, int& pos, int& bracketsCount){
-        std::pair<int,int> result = {-1,-1};
+    typedef std::pair<int,int> IntPair;
+    struct regExInfo{
 
-        while((unsigned int)pos < regExp.size()){
+        const std::string& regExp;
+        char ch;
+        unsigned int pos = 0;
+        IntPair nodes = {-1,1}; /// first and last states of current expression.
+        int bracketsCount = 0;
+        regExInfo(const std::string& regExp) : regExp(regExp){}
+    };
+    bool KleeneStar(regExInfo& info, bool lastElement = true){
+        if(info.ch != '*' )
+            return false;
 
-            char ch = regExp[pos];
-            pos++;
+        if(info.nodes.second == -1)
+            throw std::invalid_argument("Starred nothing!");
 
-            if(ch == '('){
-                bracketsCount++;
-                std::pair<int,int> nodes = readRegex(regExp,pos,bracketsCount);
-                result.second = nodes.second;
-                if(result.first == -1)
-                    result.first = nodes.first;
-            }
+        IntPair starredNodes;
 
+        if(lastElement) /// if you star only the last element.
+            starredNodes = {info.nodes.second - 1, info.nodes.second};
+        else
+            starredNodes = info.nodes;
 
-            if(ch == ')'){
-                bracketsCount--;
-                if(bracketsCount < 0)
-                    throw std::invalid_argument("Wrong parenthesis!");
-                break;
-            }
+        edges[starredNodes.second][LAMBDA].insert(starredNodes.first);
 
+        edges[starredNodes.first][LAMBDA].insert(starredNodes.second);
+        return true;
+    }
+    bool openBracket(regExInfo& info){
+        if(info.ch != '(')
+            return false;
+        info.bracketsCount++;
 
-            if(ch == '*' || ch == '+'){
-                if(result.second == -1)
-                    throw std::invalid_argument("Starred nothing!");
+        IntPair oldNodes = info.nodes;
+        readRegex(info);
 
-                edges[result.second][LAMBDA].insert(result.first);
+        if(oldNodes.first != -1)
+            info.nodes.first = oldNodes.first;
+        return true;
+    }
+    bool closeBracket(regExInfo& info){
+        if(info.ch != ')')
+            return false;
 
-                if(ch == '*')
-                    edges[result.first][LAMBDA].insert(result.second);
-            }
-            if((ch >= 'A' && ch <='Z') ||
-               (ch >= 'a' && ch <='z') ||
-               (ch >= '0' && ch <='9')){
+        info.bracketsCount--;
+        if(info.bracketsCount < 0)
+            throw std::invalid_argument("Wrong parenthesis!");
 
-                if(result.second == -1)
-                    result.second = pos;
-
-                if(result.first == -1 || bracketsCount == 0)
-                    result.first = result.second;
-
-                edges[result.second][ch].insert(result.second+1);
-                result.second++;
-
-            }
+        if(info.pos < info.regExp.size() && info.regExp[info.pos] == '*'){
+            info.ch = info.regExp[info.pos];
+            KleeneStar(info,false);
+            info.pos++;
         }
-        return result;
 
+        return true;
+    }
+    bool letter(regExInfo& info){
+        if(!acceptedLetter(info.ch))
+             return false;
 
+        if(info.nodes.first == -1)
+            info.nodes.first = info.nodes.second;
+
+        edges[info.nodes.second][info.ch].insert(info.nodes.second+1);
+        info.nodes.second++;
+
+        return true;
+    }
+    void readRegex(regExInfo& info){
+        info.nodes.first = -1;
+
+        while(info.pos < info.regExp.size()){
+
+            info.ch = info.regExp[info.pos];
+            info.pos++;
+
+            if(openBracket(info))
+                continue;
+
+            if(closeBracket(info))
+                break;
+
+            KleeneStar(info); /// since not in parenthesis, star only last element
+
+            letter(info);
+
+        }
     }
 public:
     l_NFA(const std::string& regExp){
-        int index = 0, bracketsCount = 0;
-        std::pair<int,int> result = readRegex(regExp, index,bracketsCount);
-        startState = result.first;
-        finalStates.insert(result.second);
+        std::string preParsedExp;
+
+        for(char ch:regExp){
+            if(ch != ' ')
+                preParsedExp.push_back(ch);
+        } /// ignore all spaces
+
+        regExInfo info(preParsedExp);
+
+
+        readRegex(info);
+
+        startState = info.nodes.first;
+        finalStates.insert(info.nodes.second);
     }
 };
 
@@ -129,7 +195,7 @@ public:
 int main(){
 std::ifstream fin("input.txt");
 std::ofstream fout("output.txt");
-l_NFA test("(ac)*");
+l_NFA test("(a* b*)  *");
 
 std::cout << test;
 }
